4a2e176a4d739f5c544b23952e8a9d70e6b5e812
Erwin Hoogerwoord <erwin@dialectic.design>
2026-01-12 21:12:51 +0100

    Genuary 2026 day 12

--- HyperobjectsMacOS/Data/GeometryGenerators/Genuary2026Generator.swift
+++ HyperobjectsMacOS/Data/GeometryGenerators/Genuary2026Generator.swift
@@ -12,13 +12,13 @@ import SwiftUI
 private var currentTextMainTitle = "Genuary"
 private var mapMainTitle: [Int: Character] = [:]
 
-private var currentTextDay = "Day 11"
+private var currentTextDay = "Day 12"
 private var mapDay: [Int: Character] = [:]
 
 private var currentTextYear = "2026"
 private var mapYear: [Int: Character] = [:]
 
-private var currentTextPrompt = "Quine."
+private var currentTextPrompt = "Boxes only."
 private var mapPrompt: [Int: Character] = [:]
 
 private var currentTextCredit = "socratism.io"
@@ -296,6 +296,9 @@ class Genuary2026Generator: CachedGeometryGenerator {
         let depthInput = scene.getInputWithName(name: "Depth")
         
         let brightnessInput = scene.getInputWithName(name: "Brightness")
+        let redInput = scene.getInputWithName(name: "Red")
+        let greenInput = scene.getInputWithName(name: "Green")
+        let blueInput = scene.getInputWithName(name: "Blue")
 
         let regenerationTriggerLevel: Float = floatFromInputs(inputs, name: "Regeneration Trigger Level")
         
@@ -1508,6 +1511,63 @@ class Genuary2026Generator: CachedGeometryGenerator {
                     }
                 }
             }
+        } else if dayNumber == "12" {
+            var cubesOutputLines: [Line] = []
+            
+            var rotationXMatrix = matrix_rotation(angle: Float(sin(timeAsFloat * 0.05)) * 0.25, axis: SIMD3<Float>(1.0, 0.0, 0.0))
+            var rotationZMatrix = matrix_rotation(angle: Float(cos(timeAsFloat * 0.05 + Double.pi * 0.0)) * 0.25, axis: SIMD3<Float>(0.0, 1.0, 0.0))
+            
+            var totalCubesCount = 20
+            var gDelay: Double = 35
+            for i in 0...totalCubesCount {
+                var t:Double = Double(Float(i) / Float(totalCubesCount))
+                let r = ensureValueIsFloat(redInput.getHistoryValue(millisecondsAgo: t * 50))
+                let g:Float = ensureValueIsFloat(greenInput.getHistoryValue(millisecondsAgo: gDelay - t * gDelay))
+                let b = ensureValueIsFloat(blueInput.getHistoryValue(millisecondsAgo: t * 126))
+                let brightness: Float = 1.0 // 0.8 + ensureValueIsFloat(brightnessInput.getHistoryValue(millisecondsAgo: t * 12)) * 0.2
+                let strokeWidth: Float = 0.7 // ensureValueIsFloat(brightnessInput.getHistoryValue(millisecondsAgo: t * 11)) * 5.0
+                
+                var cubeColor =  SIMD4<Float>(
+                    r * brightness,
+                    g * brightness,
+                    b * brightness,
+                    r + g + b
+                )
+                
+                var scalingMatrix = matrix_scale(scale: SIMD3<Float>(
+                    (sin(Float(t * Double.pi * 0.9 + timeAsFloat * 0.05) * 3.333) * 0.5 + 0.5) * 2,
+                    (cos(Float(t * Double.pi * 0.8 + timeAsFloat * 0.08) * 5.333) * 0.5 + 0.5) * 2,
+                    0.1
+                ))
+                
+                var cubeSize:Float = Float(t) * 0.05 + 0.6
+                cubeSize += r * 0.01
+                cubeSize -= g * 0.05
+                cubeSize += b * 0.01
+                let cubePosition:SIMD3<Float> = SIMD3<Float>(
+                    0.0,
+                    0.0,
+                    (Float(t) - 0.5) * 1.0
+                )
+                let cube = Cube(center: cubePosition, size: cubeSize)
+                var cubeLines = cube.wallOutlines()
+                for j in cubeLines.indices {
+                    cubeLines[j] = cubeLines[j].applyMatrix(scalingMatrix)
+                    cubeLines[j] = cubeLines[j].setBasicEndPointColors(startColor: cubeColor, endColor: cubeColor)
+                    cubeLines[j].lineWidthStart = strokeWidth * lineWidthBase
+                    cubeLines[j].lineWidthEnd = strokeWidth * lineWidthBase
+                }
+                
+                cubesOutputLines.append(contentsOf: cubeLines)
+            }
+            
+            for i in cubesOutputLines.indices {
+                cubesOutputLines[i] = cubesOutputLines[i].applyMatrix(rotationZMatrix)
+                cubesOutputLines[i] = cubesOutputLines[i].applyMatrix(rotationXMatrix)
+                
+            }
+            
+            lines.append(contentsOf: cubesOutputLines)
         }
         
         // return lines
--- HyperobjectsMacOS/Data/Scenes/GeometrySceneGenuary2026.swift
+++ HyperobjectsMacOS/Data/Scenes/GeometrySceneGenuary2026.swift
@@ -28,14 +28,14 @@ func generateGeometrySceneGenuary2026() -> GeometriesSceneBase {
     var scene = GeometriesSceneBase(
         name: "Genuary 2026",
         inputs: [
-            SceneInput(name: "Day", type: .string, inputGroupName: "Day configs", value: "11", presetValues: [
+            SceneInput(name: "Day", type: .string, inputGroupName: "Day configs", value: "12", presetValues: [
                 "1":"1", "2":"2", "3":"3", "4":"4", "5":"5", "6":"6", "7":"7", "8":"8", "9":"9", "10":"10",
                 "11":"11", "12":"12", "13":"13", "14":"14", "15":"15", "16":"16", "17":"17", "18":"18", "19":"19", "20":"20",
                 "21":"21", "22":"22", "23":"23", "24":"24", "25":"25", "26":"26", "27":"27", "28":"28", "29":"29", "30":"30", "31":"31"
             ]),
             SceneInput(name: "Main title", type: .string, inputGroupName: "Texts", value: "Genuary"),
             SceneInput(name: "Year", type: .string, inputGroupName: "Texts", value: "2026"),
-            SceneInput(name: "Prompt", type: .string, inputGroupName: "Texts", value: "Quines."),
+            SceneInput(name: "Prompt", type: .string, inputGroupName: "Texts", value: "Boxes only."),
             SceneInput(name: "Credit", type: .string, inputGroupName: "Texts", value: "socratism.io"),
             
             
@@ -62,6 +62,10 @@ func generateGeometrySceneGenuary2026() -> GeometriesSceneBase {
             
             SceneInput(name: "Brightness", type: .float, value: 0.0, range: 0.0...1.0, audioAmplificationAddition: 1.0),
             
+            SceneInput(name: "Red", type: .float, value: 0.0, range: 0.0...3.0, audioAmplificationAddition: 1.0),
+            SceneInput(name: "Green", type: .float, value: 0.0, range: 0.0...3.0, audioAmplificationAddition: 1.0, audioDelay: 0.1),
+            SceneInput(name: "Blue", type: .float, value: 0.0, range: 0.0...3.0, audioAmplificationAddition: 1.0, audioDelay: 0.0),
+            
             SceneInput(name: "Width", type: .float, value: 1.0, range: 0.0...3.0, audioAmplificationAddition: -0.8),
             SceneInput(name: "Height", type: .float, value: 1.0, range: 0.0...3.0, audioAmplificationAddition: -0.8, audioDelay: 0.1),
             SceneInput(name: "Depth", type: .float, value: 0.0, range: 0.0...3.0, audioAmplificationAddition: 1.0, audioDelay: 0.0),
@@ -77,7 +81,7 @@ func generateGeometrySceneGenuary2026() -> GeometriesSceneBase {
     )
     
     scene.sceneHasBackgroundColor = true
-    scene.backgroundColor = SIMD3<Float>(0.1, 0.1, 0.1)
+    scene.backgroundColor = SIMD3<Float>(1.0, 0.3, 0.0)
     
     return scene
 }
